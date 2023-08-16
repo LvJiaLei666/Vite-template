f5b496def8db14ed18ca52542b9ca6ba94c51194
lvjialei <12415976359@qq.com>
2023-08-16 15:41:23 +0800

    提交

--- src/views/About/components/VirtualScrollTable.vue
+++ src/views/About/components/VirtualScrollTable.vue
@@ -5,30 +5,33 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-const count = ref(0)
-const load = () => {
-  count.value += 2
-}
+  import { ref } from 'vue'
+
+  const count = ref(0)
+  const load = () => {
+    count.value += 2
+  }
 </script>
 
 <style>
-.infinite-list {
-  height: 300px;
-  padding: 0;
-  margin: 0;
-  list-style: none;
-}
-.infinite-list .infinite-list-item {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 50px;
-  background: var(--el-color-primary-light-9);
-  margin: 10px;
-  color: var(--el-color-primary);
-}
-.infinite-list .infinite-list-item + .list-item {
-  margin-top: 10px;
-}
+  .infinite-list {
+    height: 300px;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  .infinite-list .infinite-list-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    background: var(--el-color-primary-light-9);
+    margin: 10px;
+    color: var(--el-color-primary);
+  }
+
+  .infinite-list .infinite-list-item + .list-item {
+    margin-top: 10px;
+  }
 </style>
